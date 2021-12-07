96ae9c9944e834e99c8df81ac6c5567be1fe6acf
Akazukin8763 <starmoon900709@gmail.com>
2021-12-07 11:56:10 +0800

    更新 Manage API uploadBook：將 SQL 新增改為 Prepared Statement，避免 SQL Injection

--- src/main/ManageModule/Manage/uploadBook.php
+++ src/main/ManageModule/Manage/uploadBook.php
@@ -19,46 +19,67 @@
         if ($title != null && $image != null && $author != null && $publisher != null && $description != null && $publish_date != null && $arrive_date != null && $category != null &&
             is_string($title) && is_string($image) && is_string($author) && is_string($publisher) && is_string($description) && is_string($publish_date) && is_string($arrive_date) && is_array($category) &&
             strlen($title) <= 64 && strlen($image) <= 256 && strlen($author) <= 16 && strlen($publisher) <= 16 && strlen($description) <= 256) {
+            
+            // 建立書籍詳細內容
+            $sql_book_detail = "INSERT INTO Book_Detail (title, image, author, publisher, description, publish_date, arrive_date)
+                            VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($sql_book_detail); 
+            $stmt->bind_param("sssssss", $title, $image, $author, $publisher, $description, $publish_date, $arrive_date);
+            $result = $stmt->execute();
+            //$result = $stmt->get_result();
+
+            // 不管內容是否建立成功，都能確保資料表中含有此書籍
+            /*
+            if ($result) { // 新增書籍內容
+                // Nothing
+            }
+            else { // 已有重複
+                // Nothing
+            }
+            */
+            
+            // 建立書籍類別
+            // 書籍類別陣列轉換
+            $category_list = '';
+            $category_value = '';
+            foreach ($category as $value) {
+                $category_list = $category_list.', '.$value;
+                $category_value = $category_value.', 1';
+            }
+
+            $sql_book_category = "INSERT INTO Book_Category (title $category_list)
+                                    VALUES ('$title' $category_value)";
+            $result = $conn->query($sql_book_category);
+
+            // 不管內容是否建立成功，都能確保資料表中含有此書籍
+            /*
+            if ($result) { // 新增書籍類別
+                // Nothing
+            }
+            else { // 已有重複
+                // Nothing
+            }
+            */
 
             // 建立書籍
-            $sql_book = "INSERT INTO Book (title, image, author, publisher, description, publish_date, arrive_date)
-                            VALUES ('$title', '$image', '$author', '$publisher', '$description', '$publish_date', '$arrive_date')";
-            
-            if (mysqli_query($conn, $sql_book)) {
-                // 查看有無必要再度新增相同書籍的類別
-                $sql_same_book = "SELECT * FROM Book WHERE title = '$title'";
-                if (mysqli_num_rows(mysqli_query($conn, $sql_same_book)) == 0) { // 暫無相同書籍
-                    // 書籍類別陣列轉換
-                    $category_list = '';
-                    $category_value = '';
-                    foreach ($category as $value) {
-                        $category_list = $category_list.', '.$value;
-                        $category_value = $category_value.', 1';
-                    }
+            $sql_book = "INSERT INTO Book (title)
+                            VALUES (?)";
+            $stmt = $conn->prepare($sql_book); 
+            $stmt->bind_param("s", $title);
+            $result = $stmt->execute();
 
-                    // 建立書籍類別
-                    $sql_book_category = "INSERT INTO Book_Category (title $category_list)
-                    VALUES ('$title' $category_value)";
+            if ($result) {
+                $sql = "SELECT book_ID
+                        FROM Book
+                        WHERE title = ? ORDER BY book_ID DESC";
+                $stmt = $conn->prepare($sql); 
+                $stmt->bind_param("s", $title);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $rows = $result->fetch_all(MYSQLI_ASSOC);
 
-                    if (mysqli_query($conn, $sql_book_category)) {
-                        // Nothing
-                    }
-                    else {
-                        echo json_encode(array('errorMsg' => '建立書籍類別時發生錯誤。'));
-                        exit;
-                    }
-                }
-                else { // 有重複書籍，不新增
-                    // Nothing
-                }
-
-                // 獲取剛建立書籍的 book_ID，最後新增的那本（可能有重複 title）
-                $sql = "SELECT book_ID FROM Book WHERE title = '$title' ORDER BY book_ID DESC";
-                $result = mysqli_query($conn, $sql);
-
-                if ($result) {
-                    $row = mysqli_fetch_assoc($result);
-                    echo json_encode(array('book_ID' => $row["book_ID"]));
+                if (count($rows) != 0) {
+                    echo json_encode(array('book_ID' => $rows[0]["book_ID"]));
                 }
                 else {
                     echo json_encode(array('errorMsg' => '獲取書籍 book_ID 時發生錯誤。'));
