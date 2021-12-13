82e3bb518e8bca7e4c04693dd6b6057199d26896
Akazukin8763 <starmoon900709@gmail.com>
2021-12-13 21:46:11 +0800

    更新 View API traceBook：將書名列入追蹤，若不存在則之後上架時將會通知

---
+++ src/main/SearchViewModule/View/traceBook.php
@@ -0,0 +1,39 @@
+<?php
+    // Include config file
+    $conn = require_once "../../config.php";
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        session_start(); 
+        $ID = $_SESSION["ID"];
+        $title = $_POST["title"];
+
+        if ($title != null &&
+            is_string($title) &&
+            strlen($title) <= 64) {
+
+            $sql = "INSERT INTO User_Trace (ID, title) 
+                    Values (?, ?)";
+            $stmt = $conn->prepare($sql); 
+            $stmt->bind_param("is", $ID, $title);
+            $result = $stmt->execute();
+
+            if ($result && $stmt->affected_rows == 1) {
+                $conn->commit();
+                echo json_encode(array('__STATUS' => 'SUCCESS'));
+            }
+            else {
+                $conn->rollback();
+                echo json_encode(array('__STATUS' => 'ERROR', 'errorMsg' => '無法追蹤相同名稱的書籍。'));
+            }
+        }
+        else {
+            echo json_encode(array('errorMsg' => '參數輸入錯誤！'));
+        }
+    }
+    else {
+        echo json_encode(array('errorMsg' => '請求無效，只允許 POST 方式訪問！'));
+    }
+
+    // Close connection
+    mysqli_close($conn);
+?>
--- src/main/SearchViewModule/View/view.js
+++ src/main/SearchViewModule/View/view.js
@@ -112,6 +112,34 @@ export function viewTrace() {
     })
 }
 
-export function traceBook() {
-
+export function traceBook(__title) {
+    if (!(0 < __title.length && __title.length <= 64)) {
+        console.log('__title 長度超出限制。');
+        return;
+    }
+    
+    $.ajax({
+        type: "POST",
+        url: "SearchViewModule/View/traceBook.php",
+        dataType: "json",
+        data: {
+            title: __title,
+        },
+        success: function(response) {
+            if (response.__STATUS) { // 回傳的 json 中含有 __STATUS
+                if (response.__STATUS == "SUCCESS") {
+                    console.log(response.__STATUS);
+                }
+                else {
+                    console.log(response.__STATUS + ": " + response.errorMsg);
+                }
+            }
+            else {
+                console.log(response.errorMsg);
+            }
+        },
+        error: function(jqXHR) {
+            console.log(jqXHR);
+        }
+    })
 }
