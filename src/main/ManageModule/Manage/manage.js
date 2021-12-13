000a3a07dc82693983a9005d6b70a4f606e94816
Akazukin8763 <starmoon900709@gmail.com>
2021-12-13 19:36:34 +0800

    更新 Manage API editBook：更改資料庫中所有相關資料

--- src/main/ManageModule/Manage/manage.js
+++ src/main/ManageModule/Manage/manage.js
@@ -15,7 +15,7 @@ export function uploadBook(__title, __image, __author, __publisher, __descriptio
         console.log('__publisher 長度超出限制。');
         return;
     }
-    else if (!(__description.length <= 256)) {
+    else if (!(__description.length <= 512)) {
         console.log('__description 長度超出限制。');
         return;
     }
@@ -49,6 +49,78 @@ export function uploadBook(__title, __image, __author, __publisher, __descriptio
         success: function(response) {
             if (response.book_ID) { // 回傳的 json 中含有 book_ID
                 console.log(response.book_ID);
+            }
+            else {
+                console.log(response.errorMsg);
+            }
+        },
+        error: function(jqXHR) {
+            console.log(jqXHR);
+        }
+    })
+}
+
+export function editBook(__old_title, __title, __image, __author, __publisher, __description, __publish_date, __arrive_date, __category) {
+    if (!(__old_title.length <= 64)) {
+        console.log('__old_title 長度超出限制。');
+        return;
+    }
+    else if (!(__title.length <= 64)) {
+        console.log('__title 長度超出限制。');
+        return;
+    }
+    else if (!(__image.length <= 256)) {
+        console.log('__image 長度超出限制。');
+        return;
+    }
+    else if (!(__author.length <= 16)) {
+        console.log('__author 長度超出限制。');
+        return;
+    }
+    else if (!(__publisher.length <= 16)) {
+        console.log('__publisher 長度超出限制。');
+        return;
+    }
+    else if (!(__description.length <= 512)) {
+        console.log('__description 長度超出限制。');
+        return;
+    }
+    else if (isNaN(Date.parse(__publish_date))) {
+        console.log('__publish_date 並非標準日期和時間格式字串。');
+        return;
+    }
+    else if (isNaN(Date.parse(__arrive_date))) {
+        console.log('__arrive_date 並非標準日期和時間格式字串。');
+        return;
+    }
+    else if (!Array.isArray(__category)) {
+        console.log('__category 並非陣列。');
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "ManageModule/Manage/editBook.php",
+        dataType: "json",
+        data: {
+            old_title: __old_title,
+            title: __title,
+            image: __image,
+            author: __author,
+            publisher: __publisher,
+            description: __description,
+            publish_date: __publish_date,
+            arrive_date: __arrive_date,
+            category: __category,
+        },
+        success: function(response) {
+            if (response.__STATUS) { // 回傳的 json 中含有 __STATUS
+                if (response.__STATUS == "SUCCESS") {
+                    console.log(response.__STATUS);
+                }
+                else {
+                    console.log(response.__STATUS + ": " + response.errorMsg);
+                }
             }
             else {
                 console.log(response.errorMsg);
