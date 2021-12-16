12efac46e593ca1cb143954537b59200e284707a
Akazukin8763 <starmoon900709@gmail.com>
2021-12-16 15:24:06 +0800

    更新 Check API reserveBook：若於第一順位預約，則更新通知日期

--- src/main/CheckModule/Check/reserveBook.php
+++ src/main/CheckModule/Check/reserveBook.php
@@ -6,7 +6,9 @@
         session_start(); 
         $ID = $_SESSION["ID"];
         $book_ID = $_POST["book_ID"];
-        
+
+        $conn->autocommit(false);
+
         if ($ID != null && $book_ID != null &&
             is_int((int) $ID) && is_int((int) $book_ID)) {
             
@@ -57,8 +59,6 @@
                 $rows_is_reserve = $result_is_reserve->fetch_all(MYSQLI_ASSOC);
 
                 if (count($rows_is_reserve) == 0) {
-                    $conn->autocommit(false);
-
                     if ($book_status != "BORROW") { // IDLE, RESERVE
                         // 將狀態改成 RESERVE
                         $sql_book = "UPDATE Book
@@ -80,14 +80,72 @@
 
                     $today = date("Y-m-d H:i:s");
 
-                    // 預約書籍，允許多人預約同本書，優先通知最早預約的使用者
-                    $sql_book_reserve = "INSERT INTO Book_Trace (book_ID, ID, start_date)
+                    // 確認是否是第一順位預約，是則發通知
+                    $sql_fisrt_reserve = "SELECT *
+                                            FROM Book_Trace
+                                            WHERE book_ID = ? AND deadline IS NULL";
+                    $stmt_fisrt_reserve = $conn->prepare($sql_fisrt_reserve); 
+                    $stmt_fisrt_reserve->bind_param("i", $book_ID);
+                    $stmt_fisrt_reserve->execute();
+                    $result_fisrt_reserve = $stmt_fisrt_reserve->get_result();
+                    $rows_fisrt_reserve = $result_fisrt_reserve->fetch_all(MYSQLI_ASSOC);
+
+                    $notify_date;
+                    if (count($rows_fisrt_reserve) != 0) { // 不是第一順位，不發通知
+                        // Nothing
+                    }
+                    else { // 第一順位，通知
+                        $title;
+                        
+                        // 查詢書籍名稱
+                        $sql_title = "SELECT title
+                                        FROM Book
+                                        WHERE book_ID = ?";
+                        $stmt_title = $conn->prepare($sql_title); 
+                        $stmt_title->bind_param("i", $book_ID);
+                        $stmt_title->execute();
+                        $result_title = $stmt_title->get_result();
+                        $rows_title = $result_title->fetch_all(MYSQLI_ASSOC);
+
+                        if (count($rows_title) == 1) { // 找的到書名
+                            $title = $rows_title[0]["title"];
+                        }
+                        else { // 找不到書名（理論上不可能）
+                            $conn->rollback();
+                            echo json_encode(array('__STATUS' => 'ERROR',
+                                                    'errorMsg' => '查無書籍資料。'));
+                            exit;
+                        }
+
+                        $notify_date = $today;
+                        $content = "成功預約書籍「".$title."（book_ID：".$book_ID."）」於第一順位，需在 3 日內前去圖書館借閱，否則取消預約資格。";
+                        
+                        // 通知
+                        $sql_notify = "INSERT INTO Notification (ID, notify_date, content)
                                             VALUES (?, ?, ?)";
+                        $stmt_notify = $conn->prepare($sql_notify);
+                        $stmt_notify->bind_param("iss", $ID, $notify_date, $content);
+                        $result_notify = $stmt_notify->execute();
+
+                        if ($result_notify && $stmt_notify->affected_rows == 1) {
+                            // Nothing
+                        }
+                        else {
+                            $conn->rollback();
+                            echo json_encode(array('__STATUS' => 'ERROR',
+                                                    'errorMsg' => '傳送通知時發生錯誤。'));
+                            exit;
+                        }
+                    }
+
+                    // 預約書籍，允許多人預約同本書，優先通知最早預約的使用者
+                    $sql_book_reserve = "INSERT INTO Book_Trace (book_ID, ID, start_date, notify_date)
+                                            VALUES (?, ?, ?, ?)";
                     $stmt_book_reserve = $conn->prepare($sql_book_reserve); 
-                    $stmt_book_reserve->bind_param("iis", $book_ID, $ID, $today);
+                    $stmt_book_reserve->bind_param("iiss", $book_ID, $ID, $today, $notify_date);
                     $result_book_reserve = $stmt_book_reserve->execute();
 
-                    if ($result_book_reserve) {
+                    if ($result_book_reserve && $stmt_book_reserve->affected_rows == 1) {
                         $conn->commit();
                         echo json_encode(array('__STATUS' => 'SUCCESS'));
                     }
